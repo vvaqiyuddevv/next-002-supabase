fa344a5e15fd09f09d0829ac6394c4b771c62b87
waqiyuddev <134506537+vvaqiyuddevv@users.noreply.github.com>
2025-11-12 23:34:14 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { ArrowUpRight, SearchIcon } from "lucide-react";
 
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
